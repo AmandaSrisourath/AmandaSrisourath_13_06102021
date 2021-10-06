4807c217a4cccb02beef4bb636006e4bbcc128bd
Amandozz <amanda.srisou@gmail.com>
2021-10-06 11:32:35 +1000

    add SignIn component

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "../Header";
+import Footer from "../Footer";
+import { Link } from "react-router-dom";
 
 class SignIn extends React.Component {
     render() {
@@ -13,23 +14,24 @@ class SignIn extends React.Component {
                         <h1>Sign In</h1>
                         <form>
                             <div className="input-wrapper">
-                                <label htmlFor="username">Username</label
-                                ><input type="text" id="username"/>
+                                <label htmlFor="username">Username</label>
+                                <input type="text" id="username"/>
                             </div>
+
                             <div className="input-wrapper">
-                                <label htmlFor="password">Password</label
-                                ><input type="password" id="password"/>
+                                <label htmlFor="password">Password</label>
+                                <input type="password" id="password"/>
                             </div>
+
                             <div className="input-remember">
-                                <input type="checkbox" id="remember-me"/><label htmlFor="remember-me"
-                            >Remember me</label
-                            >
+                                <input type="checkbox" id="remember-me"/>
+                                <label htmlFor="remember-me">Remember me</label>
                             </div>
-                            {/*// <!-- PLACEHOLDER DUE TO STATIC SITE -->*/}
-                            {/*// <a href="./user.html" className="sign-in-button">Sign In</a>*/}
-                            {/*// <!-- SHOULD BE THE BUTTON BELOW -->*/}
-                            {/*// <!-- <button class="sign-in-button">Sign In</button> -->*/}
-                            {/*// <!--  -->*/}
+
+                            <Link to={`/user`}>
+                                <button className="sign-in-button">Sign In</button>
+                            </Link>
+
                         </form>
                     </section>
                 </main>
