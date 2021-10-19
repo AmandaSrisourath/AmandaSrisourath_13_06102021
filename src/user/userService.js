071b7a7b70f178f777478244fc4a07b366ad2e12
Amandozz <amanda.srisou@gmail.com>
2021-10-19 11:56:00 +1000

    add api call getInfos

--- src/user/userService.js
+++ src/user/userService.js
@@ -9,19 +9,26 @@ function login (email, password) {
         email,
         password
     }).then(response => {
-        console.log(response);
+        // console.log(response);
         return response?.data?.body?.token;
     }).catch(error => {
-        console.log(error);
+        // console.log(error);
         return error;
         })
 }
 
 function getInfos (token) {
     return client.post(`/user/profile`, {
-        header: {
+    }, {
+        headers: {
             'Authorization': `Bearer ${token}`
         }
+    }).then(response => {
+        // console.log(response.data.body);
+        return response?.data?.body;
+    }).catch(error => {
+        // console.log(error);
+        return error;
     })
 }
 
