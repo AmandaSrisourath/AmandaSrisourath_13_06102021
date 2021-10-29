754251dab6b600e9124230a61f6f4bb1a30af4e8
Amandozz <amanda.srisou@gmail.com>
2021-10-29 16:38:06 +1000

    add persistor

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import { store } from "./utils/store";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from "./utils/store";
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </React.StrictMode>
     </Provider>,
   document.getElementById('root')
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import userReducer from '../user/userSlice';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const reducers = combineReducers({
+    user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
+    reducer: persistedReducer,
 })
+
+export const persistor = persistStore(store);
